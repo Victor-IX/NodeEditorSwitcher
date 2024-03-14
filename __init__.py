9d1c3912a7a7b66b77136606c77157fc7775c2e8
Victor <victor.ched@gmail.com>
2024-03-13 23:29:39 -0400

    Adding Pie Menu and Shortcut

--- __init__.py
+++ __init__.py
@@ -45,6 +45,20 @@ class SwitchGeometryEditor(bpy.types.Operator):
         bpy.context.area.ui_type = 'GeometryNodeTree'
         return {'FINISHED'}
 
+class VIEW3D_MT_NODE_PIE_Menu(bpy.types.Menu):
+    bl_idname = "VIEW3D_MT_NODE_PIE_Menu"
+    bl_label = "Node Editor Pie Menu"
+
+    def draw(self, context):
+        layout = self.layout
+        pie = layout.menu_pie()
+
+        pie.operator("node.switch_to_shader_editor", text="Shader", icon="NODE_MATERIAL")
+        pie.operator("node.switch_to_world_editor", text="World", icon="WORLD_DATA")
+        pie.operator("node.switch_to_geometry_editor", text="Geometry", icon="GEOMETRY_NODES")
+        pie.operator("node.switch_to_compositor_editor", text="Compositor", icon="NODE_COMPOSITING")
+
+
 def draw_switch_buttons(self, context):
     layout = self.layout
 
@@ -81,13 +95,25 @@ def register():
     bpy.utils.register_class(SwitchCompositorEditor)
     bpy.utils.register_class(SwitchWorldEditor)
     bpy.utils.register_class(SwitchGeometryEditor)
+    bpy.utils.register_class(VIEW3D_MT_NODE_PIE_Menu)
     bpy.types.NODE_HT_header.append(draw_switch_buttons)
+    
+    wm = bpy.context.window_manager
+    km = wm.keyconfigs.addon.keymaps.new(name='Node Editor', space_type='NODE_EDITOR')
+    kmi = km.keymap_items.new('wm.call_menu_pie', 'SEMI_COLON', 'PRESS', ctrl=True, alt=True)
+    kmi.properties.name = "VIEW3D_MT_NODE_PIE_Menu"
+    
+    kmi = km.keymap_items.new('node.switch_to_geometry_editor', 'ONE', 'PRESS')
+    kmi = km.keymap_items.new('node.switch_to_shader_editor', 'TWO', 'PRESS')
+    kmi = km.keymap_items.new('node.switch_to_world_editor', 'THREE', 'PRESS')
+    kmi = km.keymap_items.new('node.switch_to_compositor_editor', 'FOUR', 'PRESS')
 
 def unregister():
     bpy.utils.unregister_class(SwitchNodeEditor)
     bpy.utils.unregister_class(SwitchCompositorEditor)
     bpy.utils.unregister_class(SwitchWorldEditor)
     bpy.utils.unregister_class(SwitchGeometryEditor)
+    bpy.utils.unregister_class(VIEW3D_MT_NODE_PIE_Menu)
     bpy.types.NODE_HT_header.remove(draw_switch_buttons)
 
 if __name__ == "__main__":
