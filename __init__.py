0e9239916e19f38409d97fb43418ef67943d31d2
Victor <victor.ched@gmail.com>
2024-03-13 22:55:04 -0400

    add active node editor button and fix spacing

--- __init__.py
+++ __init__.py
@@ -47,32 +47,34 @@ class SwitchGeometryEditor(bpy.types.Operator):
 
 def draw_switch_buttons(self, context):
     layout = self.layout
-    row = layout.row()
-    row.prop(context.space_data, "use_nodes")
 
     if context.area.ui_type == 'ShaderNodeTree' and context.space_data.shader_type == 'OBJECT':
         row = layout.row(align=True)
-        row.operator("node.switch_to_world_editor", text="", icon="WORLD_DATA")
         row.operator("node.switch_to_geometry_editor", text="", icon="GEOMETRY_NODES")
+        row.operator("node.switch_to_shader_editor", text="", icon="NODE_MATERIAL", emboss=True, depress=True)
+        row.operator("node.switch_to_world_editor", text="", icon="WORLD_DATA")
         row.operator("node.switch_to_compositor_editor", text="", icon="NODE_COMPOSITING")
     
     if context.area.ui_type == 'ShaderNodeTree' and context.space_data.shader_type == 'WORLD':
         row = layout.row(align=True)
-        row.operator("node.switch_to_shader_editor", text="", icon="NODE_MATERIAL")
         row.operator("node.switch_to_geometry_editor", text="", icon="GEOMETRY_NODES")
+        row.operator("node.switch_to_shader_editor", text="", icon="NODE_MATERIAL")
+        row.operator("node.switch_to_world_editor", text="", icon="WORLD_DATA", emboss=True, depress=True)
         row.operator("node.switch_to_compositor_editor", text="", icon="NODE_COMPOSITING")
     
     if context.area.ui_type == 'GeometryNodeTree':
         row = layout.row(align=True)
+        row.operator("node.switch_to_geometry_editor", text="", icon="GEOMETRY_NODES", emboss=True, depress=True)
         row.operator("node.switch_to_shader_editor", text="", icon="NODE_MATERIAL")
         row.operator("node.switch_to_world_editor", text="", icon="WORLD_DATA")
         row.operator("node.switch_to_compositor_editor", text="", icon="NODE_COMPOSITING")
     
     if context.area.ui_type == 'CompositorNodeTree':
         row = layout.row(align=True)
-        row.operator("node.switch_to_shader_editor", text="", icon="NODE_MATERIAL")
         row.operator("node.switch_to_geometry_editor", text="", icon="GEOMETRY_NODES")
+        row.operator("node.switch_to_shader_editor", text="", icon="NODE_MATERIAL")
         row.operator("node.switch_to_world_editor", text="", icon="WORLD_DATA")
+        row.operator("node.switch_to_compositor_editor", text="", icon="NODE_COMPOSITING", emboss=True, depress=True)
     
 def register():
     bpy.utils.register_class(SwitchNodeEditor)
